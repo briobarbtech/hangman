8c490a3ca1c523fe1a313df7cfdb097ebaba9ec9
Brian Barquesi <barquesi.tech@gmail.com>
2022-06-15 15:54:58 -0300

    agregando función que define que se hace si la palabra es correcta

--- ahorcado/js/main.js
+++ ahorcado/js/main.js
@@ -24,6 +24,17 @@ let hits;
 
 
 
+const correctLetter = letter => {
+    const { children } =wordContainer;
+    for(let i = 0; i < children.length; i++) {
+        if(children[i].innerHTML === letter) {
+            children[i].classList.toggle('hidden');
+            hits++
+        }
+    }
+    if (hits === selectedWord.length) endGame();
+}
+
 const letterInput = letter => {
     if(selectedWord.includes(letter)) {
         correctLetter(letter);
